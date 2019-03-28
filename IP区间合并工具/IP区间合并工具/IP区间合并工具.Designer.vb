faf6affaaf5ddca235a4d7f83d5510d8d1842d9f
御剑 <49001550+foryujian@users.noreply.github.com>
2019-03-29 00:32:06 +0800

    Add files via upload

---
+++ IP区间合并工具/IP区间合并工具/IP区间合并工具.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class IP区间合并工具
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(282, 107)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(274, 257)
+        Me.RichTextBox2.TabIndex = 0
+        Me.RichTextBox2.Text = ""
+        Me.RichTextBox2.WordWrap = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(481, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 60)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "合并"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "IP文件路径(支持文件拖拽)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.Location = New System.Drawing.Point(14, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(461, 21)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(282, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "区间合并结果预览"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(17, 107)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(259, 257)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.WordWrap = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "原文件内容预览"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 48)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 12)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "IP区间合并后的文件"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(14, 63)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(461, 21)
+        Me.TextBox2.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(568, 376)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(584, 415)
+        Me.MinimumSize = New System.Drawing.Size(584, 415)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "IP区间合并工具（御剑数据处理工具集）"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ IP区间合并工具/IP区间合并工具/IP区间合并工具.vb
@@ -0,0 +1,155 @@
+﻿Imports System.Text
+
+Public Class IP区间合并工具
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.TextLength > 0 Then
+            If IO.File.Exists(TextBox1.Text) Then
+                Button1.Enabled = False
+                Threading.ThreadPool.QueueUserWorkItem(AddressOf go, TextBox1.Text)
+            End If
+        End If
+    End Sub
+
+    Private Sub go(ByVal file As String)
+        Dim intervals As New List(Of Interval)
+        Dim tmp() As String
+        Dim ip(3) As Integer
+        Dim st, ed As Long
+        Dim i As Integer
+        For Each txt In IO.File.ReadLines(file)
+            If txt.Length = 0 Then Continue For
+            If txt.Contains("-") Then
+                tmp = txt.Split("-")
+                If tmp.Length = 2 Then
+                    st = IpToLong(tmp(0))
+                    ed = IpToLong(tmp(1))
+                    intervals.Add(New Interval(st, ed))
+                End If
+            ElseIf txt.Contains("/") Then
+                tmp = txt.Split("/")
+                If tmp.Length = 2 Then
+                    Dim ip1 As String = tmp(0)
+                    Dim ip2 As String = tmp(1)
+                    tmp = ip1.Split(".")
+                    For x As Integer = 0 To 3
+                        ip(x) = Integer.Parse(tmp(x))
+                        tmp(x) = Convert.ToString(ip(x), 2).PadLeft(8, "0")
+                    Next
+                    Dim BitString As String = String.Join("", tmp)
+                    BitString = BitString.Substring(0, CInt(ip2)).PadRight(32, "1")
+                    i = 0
+                    For x As Integer = 0 To 31 Step 8
+                        tmp(i) = BitString.Substring(x, 8)
+                        ip(i) = Convert.ToInt32(tmp(i), 2)
+                        i += 1
+                    Next
+                    ip2 = String.Join(".", ip)
+                    st = IpToLong(ip1)
+                    ed = IpToLong(ip2)
+                    intervals.Add(New Interval(st, ed))
+                End If
+            Else
+                intervals.Add(New Interval(IpToLong(txt), IpToLong(txt)))
+            End If
+        Next
+        Dim fi As New IO.FileInfo(file)
+        Dim newfile As String = fi.DirectoryName & "\New_" & fi.Name
+        Dim IPRes = Merge(intervals)
+        Dim appstr As New System.Text.StringBuilder
+        For Each item In IPRes
+            If item.st <> item.ed Then
+                appstr.AppendLine(LongToIP(item.st) & "-" & LongToIP(item.ed))
+            Else
+                appstr.AppendLine(LongToIP(item.st))
+            End If
+        Next
+        IO.File.WriteAllText(newfile, appstr.ToString)
+        Invoke(Sub()
+                   TextBox2.Text = newfile
+                   RichTextBox2.Text = YuLan(newfile)
+                   Button1.Enabled = True
+               End Sub)
+    End Sub
+
+    Public Shared Function IpToLong(strIP As String) As Long
+        Try
+            Dim ip(3) As Long
+            Dim s As String() = strIP.Split(".")
+            ip(0) = Long.Parse(s(0))
+            ip(1) = Long.Parse(s(1))
+            ip(2) = Long.Parse(s(2))
+            ip(3) = Long.Parse(s(3))
+            Return (ip(0) << 24) + (ip(1) << 16) + (ip(2) << 8) + ip(3)
+        Catch ex As Exception
+            Return 0
+        End Try
+    End Function
+
+    Public Shared Function LongToIP(longIP As Long) As String
+        Dim sb As New StringBuilder
+        sb.Append(longIP >> 24)
+        sb.Append(".")
+        sb.Append((longIP And &HFFFFFF) >> 16)
+        sb.Append(".")
+        sb.Append((longIP And &HFFFF) >> 8)
+        sb.Append(".")
+        sb.Append((longIP And &HFF))
+        Return sb.ToString()
+    End Function
+
+    Public Function Merge(ByVal intervals As List(Of Interval)) As List(Of Interval)
+        Dim res As New List(Of Interval)
+        If intervals.Count = 0 Then Return res
+        intervals = intervals.OrderBy(Function(i) i.st).ToList
+        res.Add(intervals(0))
+        For i As Long = 1 To intervals.Count - 1
+            If intervals(i).st <= res(res.Count - 1).ed Then
+                res(res.Count - 1).ed = Math.Max(intervals(i).ed, res(res.Count - 1).ed)
+            Else
+                res.Add(intervals(i))
+            End If
+        Next
+        Return res
+    End Function
+
+    Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
+        TextBox1.Text = e.Data.GetData(DataFormats.FileDrop).getvalue(0).ToString
+        RichTextBox1.Text = YuLan(TextBox1.Text)
+    End Sub
+
+    Private Function YuLan(ByVal file As String) As String
+        Dim appstr As New System.Text.StringBuilder
+        If IO.File.Exists(file) Then
+            Dim x As Integer
+            For Each s In IO.File.ReadLines(file)
+                If s.Length = 0 Then Continue For
+                x += 1
+                If x > 100 Then Exit For
+                appstr.AppendLine(s)
+            Next
+        End If
+        Return appstr.ToString
+    End Function
+
+    Private Sub TextBox1_DragEnter(sender As Object, e As DragEventArgs) Handles TextBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop, False) = True Then
+            e.Effect = DragDropEffects.Copy '指针变成+号，表示正在拖入数据
+        End If
+    End Sub
+
+End Class
+
+Public Class Interval
+    Public st As Long
+    Public ed As Long
+
+    Public Sub New()
+        st = 0
+        ed = 0
+    End Sub
+
+    Public Sub New(ByVal s As Long, ByVal e As Long)
+        st = s
+        ed = e
+    End Sub
+End Class
